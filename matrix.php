7c5999f0248dec40e7992806167ea5bbee3599bd
Donnie West <donnie.west@grizzard.com>
2014-06-02 10:35:09 -0400

    Matrix Load

--- matrix.php
+++ matrix.php
@@ -1,4 +1,27 @@
-<?php include('header.php'); ?>
+<?php include( 'header.php'); ?>
 
 
-<?php include('footer.php'); ?>
+<table class="table table-striped table-condensed">
+    <thead>
+        <tr>
+            <th>th is 0</th>
+            <th>th is 1</th>
+            <th>th is 2</th>
+            <th>th is 3</th>
+            <th>th is 4</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>cell is row 0, column 0</td>
+            <td>cell is row 0, column 1</td>
+            <td>cell is row 0, column 2</td>
+            <td>cell is row 0, column 3</td>
+            <td>cell is row 0, column 4</td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+<?php include( 'footer.php'); ?>
